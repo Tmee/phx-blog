3c89d18ec8eab34399ef422e7150ab7b6e1eae8e
Tim <tjmee90@gmail.com>
2024-06-10 09:06:06 -0600

    1

--- lib/phx_blog_web/router.ex
+++ lib/phx_blog_web/router.ex
@@ -8,6 +8,7 @@ defmodule PhxBlogWeb.Router do
     plug :put_root_layout, html: {PhxBlogWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug PhxBlogWeb.Plugs.WWWRedirect
   end
 
   pipeline :api do
